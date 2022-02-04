70e05e84cf215f4980e66a2a6b49fe13fc560e3c
Kallum Jones <kallum.jones@autotrader.co.uk>
2022-02-04 08:28:34 +0000

    Add working copy of game

--- src/main.js
+++ src/main.js
@@ -25,44 +25,75 @@ function checkCorrectWord(word) {
     return word === correctWord;
 }
 
+function focus() {
+    //TO DO
+}
+
 // Grab Dom Elements needed
 const words = document.querySelectorAll('.word');
 const wordContainer = document.querySelector('.words-container');
-const wordOneLetterFive = document.getElementById('word-one__letter-five');
-const wordTwoLetterFive = document.getElementById('word-two__letter-five');
+const letterFives = document.querySelectorAll('.letter_five');
 
 // Set up
 const correctWord = generateWord();
+let currentWordIndex = 0;
 console.log(correctWord);
 
 //Check full words
-wordOneLetterFive.addEventListener('keyup', (event) => {
-    const parentWord = words[0].querySelectorAll('.word-one');
-    const nextWord = words[1].querySelectorAll('.word-two');
-    console.log(parentWord)
-    const enteredValue = getWord(parentWord);
+letterFives.forEach(letter => {
+    letter.addEventListener('keyup', () => {
+        if(currentWordIndex === 4){
+            const parentWord = words[currentWordIndex].querySelectorAll('.letter');
+            const enteredValue = getWord(parentWord);
+            parentWord.forEach((letter, index) => {
+                const currentLetter = letter.value
+                if(checkLetter(currentLetter, index) === 'green') {
+                    letter.classList.add('green');
+                } else if (checkLetter(currentLetter, index) === 'orange') {
+                    letter.classList.add('orange');
+                }
+            
+                letter.disabled = true;
+            })
 
-    parentWord.forEach((letter, index) => {
-        const currentLetter = letter.value
-        if(checkLetter(currentLetter, index) === 'green') {
-            letter.classList.add('green');
-        } else if (checkLetter(currentLetter, index) === 'orange') {
-            letter.classList.add('orange');
+            if(!checkCorrectWord(enteredValue)) {
+                const losingMessage = document.createElement('h1');
+                losingMessage.innerText = `Word: ${correctWord} Try Again!`;
+                wordContainer.appendChild(losingMessage);
+            } else if (checkCorrectWord(enteredValue)) {
+                const winningMessage = document.createElement('h1');
+                winningMessage.innerText = `Well Done You Win!`;
+                wordContainer.appendChild(winningMessage);
+            }
+        } else {
+            const parentWord = words[currentWordIndex].querySelectorAll('.letter');
+            const nextWord = words[currentWordIndex + 1].querySelectorAll('.letter');
+            const enteredValue = getWord(parentWord);
+            
+            parentWord.forEach((letter, index) => {
+                const currentLetter = letter.value
+                if(checkLetter(currentLetter, index) === 'green') {
+                    letter.classList.add('green');
+                } else if (checkLetter(currentLetter, index) === 'orange') {
+                    letter.classList.add('orange');
+                }
+            
+                letter.disabled = true;
+            })
+            
+            if(!checkCorrectWord(enteredValue)) {
+                nextWord.forEach(letter => {
+                    letter.disabled = false;
+                })
+            } else if (checkCorrectWord(enteredValue)) {
+                const winningMessage = document.createElement('h1');
+                winningMessage.innerText = `Well Done You Win!`;
+                wordContainer.appendChild(winningMessage);
+            }
+            
+            console.log(checkCorrectWord(enteredValue));
+            console.log(currentWordIndex);
+            currentWordIndex++;
         }
-
-        letter.disabled = true;
     })
-
-    if(!checkCorrectWord(enteredValue)) {
-        nextWord.forEach(letter => {
-            letter.disabled = false;
-        })
-    } else if (checkCorrectWord(enteredValue)) {
-        const winningMessage = document.createElement('h1');
-        winningMessage.innerText = `Well Done You Win!`;
-        wordContainer.appendChild(winningMessage);
-    }
-
-
-    console.log(checkCorrectWord(enteredValue));
-});
+})
